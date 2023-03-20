5a8181aa381d01ce476a91185dba3268bb4453f2
BrightonMboya <b.mboya@alustudent.com>
2023-03-20 14:37:55 +0200

    mobile design for tonero

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+        playFair: ["Playfair Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
